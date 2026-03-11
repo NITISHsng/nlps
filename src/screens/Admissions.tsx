7439b2b0bc75de1cafdb1bd819e9fa78a73455e9
NITISHsng <singhanitish611@gmail.com>
2026-03-11 18:00:13 +0530

    fix: resolve parsing error in Admissions.tsx

--- src/screens/Admissions.tsx
+++ src/screens/Admissions.tsx
@@ -255,7 +255,6 @@ export default function Admissions() {
                   </button>
                 </div>
               </div>
-              </div>
             </div>
           </div>
         </div>
